41bd17ff7b17da537b1efa6ac7f239d47ea55125
TheElesty <32326985+TheElesty@users.noreply.github.com>
2018-10-31 18:53:03 +0100

    Add files via upload

---
+++ student_script.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @id           student.script@elesty
+// @name         student_script
+// @version      1.0
+// @namespace    https://github.com/TheElesty/student_script
+// @updateURL    https://github.com/TheElesty/student_script/raw/master/student_script.user.js
+// @downloadURL  https://github.com/TheElesty/student_script/raw/master/student_script.user.js
+// @description  StudentAgency ticket reservation script
+// @author       Elesty
+// @include      https://jizdenky.regiojet.cz/*
+// @include      http://jizdenky.regiojet.cz/*
+// @grant        none
+// ==/UserScript==
